49b6fe7515e58a33e3fe12748c988f6e9e4e6ffa
mbrandse <mbrandse@users.noreply.github.com>
2023-05-28 21:27:12 +0900

    Add files via upload

---
+++ Plugins/RamaMeleeWeapon/Source/RamaMeleePlugin/RamaMeleePlugin.Build.cs
@@ -0,0 +1,36 @@
+// Copyright 2015 by Nathan "Rama" Iyer. All Rights Reserved.
+using UnrealBuildTool;
+
+public class RamaMeleePlugin : ModuleRules
+{
+	public RamaMeleePlugin(ReadOnlyTargetRules Target) : base(Target)
+	{
+        PrivatePCHHeaderFile = "Private/RamaMeleePluginPrivatePCH.h";
+
+        PCHUsage = PCHUsageMode.UseSharedPCHs;
+		
+        PublicDependencyModuleNames.AddRange(
+			new string[] { 
+				"Core", 
+				"CoreUObject", 
+				"Engine", 
+				"InputCore",
+				"PhysicsCore", 
+                "PhysX"
+			}
+		);
+		 
+		//APEX EXCLUSIONS
+		if (Target.Platform != UnrealTargetPlatform.Android && Target.Platform != UnrealTargetPlatform.HTML5 && Target.Platform != UnrealTargetPlatform.IOS)
+		{
+			PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"APEX"
+			}
+			);
+		}
+	}
+	 
+	
+}
